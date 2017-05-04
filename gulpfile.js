10fd03cdbd34a016bd544aa9b9967c7a33dd4b5a
k-strouse <kstrouse09@gmail.com>
2017-05-04 13:34:32 -0500

    Converted CSS to SCSS, compiled it using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,13 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	maps = require('gulp-sourcemaps'),
 	 del = require('del');
+
+	 
+
+gulp.task('compileSass', function(){
+	return gulp.src("scss/application.scss")
+		.pipe(maps.init())
+		.pipe(sass())
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest('css'));
+});
