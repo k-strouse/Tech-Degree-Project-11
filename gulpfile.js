3f4d99151673f4b4eae648f3f4b933cd9f3e6b54
k-strouse <kstrouse09@gmail.com>
2017-05-05 11:03:14 -0500

    Added more to gulpfile.js to concat & minify JS code
    Added more to gulpfile.js to concat & minify JS code, sent production
    files to distribution folder

--- gulpfile.js
+++ gulpfile.js
@@ -17,3 +17,38 @@ gulp.task('compileSass', function(){
 		.pipe(maps.write('./'))
 		.pipe(gulp.dest('css'));
 });
+
+gulp.task("concatScripts", function() {
+	return gulp.src([
+		'js/jquery.js',
+		'js/fastclick.js',
+		'js/foundation.js',
+		'js/foundation.equalizer.js',
+		'js/foundation.reveal.js',
+		'js/scripts.js'])
+	.pipe(maps.init())
+	.pipe(concat("app.js"))
+	.pipe(maps.write('./'))
+	.pipe(gulp.dest("js"));
+});
+
+gulp.task("minifyScripts", ["concatScripts"] /*Added concat Scripts as a dependency of minify scripts*/, function(){
+	return gulp.src("js/app.js")
+		.pipe(uglify())
+		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest('js'));
+});
+
+gulp.task('clean', function() {
+	del(['dist', 'css/application.css*', 'js/app*.js*']);
+});
+
+gulp.task("build", ['minifyScripts', 'compileSass'], function() {
+	return gulp.src(["css/application**", "js/app.min.js",
+					"img/**", "fonts/**"], { base: './'})
+			.pipe(gulp.dest('dist'));	
+});
+
+gulp.task("default", ['clean'], function() {
+	gulp.start('build');
+});
